e3411b463b18a4f1a41c62e946de429bf4e5146a
Aaron Brown <abrown@amplify.com>
2015-06-09 20:16:02 -0400

    Move the metrics collection into a Rufus job

---
+++ jobs/pull_metrics.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+require_relative '../lib/common'
+
+SCHEDULER.every '1m', first_in: 0 do
+  now = Time.new
+  today = now.to_date
+  history = Egauge::History.new(DB, register_names: %w(use gen))
+  epoch = history.epoch
+  last_sync_time = history.last_sync_time
+  start_date = last_sync_time.nil? ? epoch.to_date : last_sync_time.to_date
+
+  if start_date == today
+    LOGGER.info "Last synced at #{last_sync_time}. Pulling new metrics until #{now}..."
+    # don't bother batching for less than a day
+    h = history.load(time_from: last_sync_time+1, time_until: now, units: Egauge::REQ_UNIT_MINUTES)
+    h.each { |register| register.write(DB) }
+  else
+    start_date.upto(Date.today) do |date|
+      start_t = date.to_time
+      # assumes second granularity
+      end_t = [(Time.new-60), ((date + 1).to_time)].min
+      LOGGER.info "Loading from #{start_t} until #{end_t}"
+
+      history = Egauge::History.new(DB)
+      h = history.load(time_from: start_t, time_until: end_t,
+                      units: Egauge::REQ_UNIT_MINUTES)
+
+      h.each { |register| register.write(DB) }
+    end
+  end
+end
--- lib/common.rb
+++ lib/common.rb
@@ -1,6 +1,8 @@
 require 'sequel'
 require 'time'
 require 'date'
+require 'logger'
+require_relative 'egauge'
 
 def generation_since(start, register_name)
   query = <<-SQL
@@ -19,5 +21,17 @@ end
 DATABASE = 'solar'
 REGISTER = 'gen'
 USAGE_REGISTER = 'use'
-DB = Sequel.connect(adapter: 'postgres', user: 'aaron', database: DATABASE)
+LOGGER = Logger.new($stderr)
+LOGGER.level = Logger::INFO
 
+Egauge.configure do |config|
+  config.url = 'http://sol.borg.lan'
+end
+
+Sequel.extension :migration
+DB = Sequel.connect(adapter: 'postgres', database: DATABASE,
+                    user: 'aaron',
+                    logger: LOGGER, sql_log_level: :debug)
+
+migration_path = File.expand_path('../../db/migrate', __FILE__)
+Sequel::Migrator.run(DB, migration_path)
